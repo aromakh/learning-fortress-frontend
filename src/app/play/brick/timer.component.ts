aa9dd49da4f467c114862f16913f4c4746054990
aromakh <andriyromakh@gmail.com>
2018-12-03 14:19:41 +0200

    #160 TimerAndIntro. IntroJS timer helps

--- src/app/play/brick/timer.component.ts
+++ src/app/play/brick/timer.component.ts
@@ -4,7 +4,7 @@ import { Timer } from "./timer.service";
 @Component({
     selector: 'timer',
     template: `
-    <div class="timer-container">
+    <div class="timer-container" [attr.data-intro]="showIntro ? 'The countdown in each section helps to keep you on track to get the work done in 20, 40 or 60 minutes. Depending on the brick type' : null " data-step="1">
         <mat-card class="timer" [ngStyle]="{color: isRed ? '#E30000' : 'black'}">
             {{ timer.timeLeft?.valueOf() / 1000 }}
         </mat-card>
@@ -30,4 +30,5 @@ import { Timer } from "./timer.service";
 export class TimerComponent {
     @Input() timer: Timer;
     @Input() isRed: boolean;
+    @Input() showIntro: boolean;
 }
