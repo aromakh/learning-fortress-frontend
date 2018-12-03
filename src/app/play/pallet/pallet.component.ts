74525e06ea001cede6d8a653e131181a1704ba93
aromakh <andriyromakh@gmail.com>
2018-12-03 13:40:06 +0200

    #160 install IntroJS. Used in PalletComponent

--- src/app/play/pallet/pallet.component.ts
+++ src/app/play/pallet/pallet.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from "@angular/core";
 import { Pallet, Brick } from "../../schema";
-import { ActivatedRoute, ParamMap } from "@angular/router";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
 import { DatabaseService } from "../../database/database.service";
 import { Observable } from "rxjs";
+import * as introJs from 'intro.js';
 
 @Component({
     selector: 'pallets',
@@ -14,14 +15,23 @@ export class PalletComponent {
     pallet: Observable<Pallet>;
     bricks: Observable<Brick[]>;
 
-    constructor(public database: DatabaseService, public route: ActivatedRoute) {
+    constructor(public database: DatabaseService, private router: Router, private route: ActivatedRoute) {
         this.route.paramMap
             .subscribe((data: ParamMap) => {
                 this.palletId = data.get('id');
-                this.pallet = database.getPallet(this.palletId)
+                this.pallet = database.getPallet(this.palletId);
                 this.pallet.subscribe((pallet) => {
                     this.bricks = database.getBricksInPallet(pallet);
-                })
-            })
+                    const bricks = this.bricks;
+                    
+                    bricks.subscribe(bricksData => {
+                        setTimeout(() => {
+                            introJs().setOption('doneLabel', 'Next page').start().oncomplete(function() {
+                                router.navigate(["/play/brick/" + bricksData[0]._ref.id]);
+                            });
+                        }, 1000);
+                    });
+                });
+            });
     }
 }
